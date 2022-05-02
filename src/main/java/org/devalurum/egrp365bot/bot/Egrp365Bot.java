3d2413eea37e42fdb8970141529b9ae1814b43b6
Alexander Urumov <sahaheh@mail.ru>
2022-05-03 01:19:31 +0800

    refactored methods

--- src/main/java/org/devalurum/egrp365bot/bot/Egrp365Bot.java
+++ src/main/java/org/devalurum/egrp365bot/bot/Egrp365Bot.java
@@ -1,6 +1,7 @@
 package org.devalurum.egrp365bot.bot;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.devalurum.egrp365bot.exception.Egrp365BotException;
 import org.devalurum.egrp365bot.model.EgrpEntity;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -10,17 +11,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.annotation.Nonnull;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import static org.devalurum.egrp365bot.utils.Constants.*;
 
+
 public class Egrp365Bot {
 
-    private final static Duration secondsToWait = Duration.ofSeconds(5);
+    private final static Duration pageLoadTimeout = Duration.ofMinutes(1);
 
     static {
         WebDriverManager.chromedriver().setup();
@@ -31,78 +33,93 @@ public class Egrp365Bot {
     public Egrp365Bot() {
         this.driver = new ChromeDriver();
         this.driver.manage().window().minimize();
+        this.driver.manage().timeouts().pageLoadTimeout(pageLoadTimeout);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(this::shutdownBot));
     }
 
-    public EgrpEntity getInfo(String cadastralNumber) {
-        driver.get(String.format(EGRP365_URL, cadastralNumber));
+    public EgrpEntity getInfo(@Nonnull String cadastralNumber) {
+        try {
+            driver.get(String.format(EGRP365_URL, cadastralNumber));
 
-        WebDriverWait wait = new WebDriverWait(driver, secondsToWait);
+            WebDriverWait wait = new WebDriverWait(driver, pageLoadTimeout);
 
-        // waiting for loading all page
-        wait.until(webDriver -> ((JavascriptExecutor) webDriver)
-                .executeScript("return document.readyState")
-                .equals("complete"));
+            // waiting for loading all page
+            wait.until(webDriver -> ((JavascriptExecutor) webDriver)
+                    .executeScript("return document.readyState")
+                    .equals("complete"));
 
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(MAIN_BLOCKS)));
-        List<WebElement> elementsMain = driver.findElements(By.cssSelector(MAIN_BLOCKS));
+            if (driver.findElements(By.className(CLASS_SHOW_MORE_INFO)).isEmpty()) {
+                throw new Egrp365BotException(String.format("Object with number:'%s' not found",
+                        cadastralNumber));
+            }
 
-        WebElement show = driver.findElement(By.className(CLASS_SHOW_MORE_INFO));
-        wait.until(ExpectedConditions.elementToBeClickable(show));
-        show.click();
+            WebElement show = driver.findElement(By.className(CLASS_SHOW_MORE_INFO));
+            wait.until(ExpectedConditions.elementToBeClickable(show));
+            show.click();
 
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(MORE_BLOCKS)));
-        List<WebElement> elementsMore = driver.findElements(By.cssSelector(MORE_BLOCKS));
+            Map<String, String> info = new LinkedHashMap<>();
 
-        Map<String, String> info = new LinkedHashMap<>();
+            setFullInfo(wait, info);
 
-        elementsMain.forEach(elem -> {
-            String key = new String(elem.findElement(By.className(MAIN_HEADER))
+            WebElement addressElem = driver.findElement(By.className(CLASS_WITH_ADDRESS));
+            WebElement coordElem = driver.findElement(By.className(CLASS_WITH_COORDINATES));
+
+            String[] coordStr = coordElem.getText().split(",");
+            double lat = Double.parseDouble(coordStr[0]);
+            double lon = Double.parseDouble(coordStr[1]);
+
+            String address = new String(addressElem
                     .getText()
                     .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
                     .intern();
 
-            String value = new String(elem.findElement(By.className(MAIN_TEXT))
-                    .getText()
-                    .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
-                    .intern();
-            info.put(key, value);
-        });
+            return EgrpEntity.builder()
+                    .cadastralNumber(cadastralNumber)
+                    .address(address)
+                    .fullInfo(info)
+                    .latitude(lat)
+                    .longitude(lon)
+                    .build();
 
-        elementsMore.forEach(elem -> {
-            String key = new String(elem.findElement(By.className(MORE_HEADER))
-                    .getText()
-                    .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
-                    .intern();
-            String value = new String(elem.findElement(By.className(MORE_TEXT))
-                    .getText()
-                    .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
-                    .intern();
-
-            info.put(key, value);
-        });
-
-        WebElement coordElem = driver.findElement(By.className(CLASS_WITH_COORDINATES));
-        WebElement addressElem = driver.findElement(By.className(CLASS_WITH_ADDRESS));
-
-        String address = new String(addressElem
-                .getText()
-                .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
-                .intern();
-
-        String[] coordStr = coordElem.getText().split(",");
-        double lat = Double.parseDouble(coordStr[0]);
-        double lon = Double.parseDouble(coordStr[1]);
-
-        return EgrpEntity.builder()
-                .cadastralNumber(cadastralNumber)
-                .address(address)
-                .latitude(lat)
-                .longitude(lon)
-                .fullInfo(info)
-                .build();
+        } catch (Exception e) {
+            shutdownBot();
+            throw new Egrp365BotException("Page parsing error.", e);
+        }
     }
 
-    public void closeBrowser() {
+    private void setFullInfo(WebDriverWait wait, Map<String, String> map) {
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(MAIN_BLOCKS)))
+                .forEach(elementMain -> {
+                    String key = new String(elementMain.findElement(By.className(MAIN_HEADER))
+                            .getText()
+                            .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
+                            .intern();
+
+                    String value = new String(elementMain.findElement(By.className(MAIN_TEXT))
+                            .getText()
+                            .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
+                            .intern();
+                    map.put(key, value);
+                });
+
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector(MORE_BLOCKS)))
+                .forEach(elementMore -> {
+                    String key = new String(elementMore.findElement(By.className(MORE_HEADER))
+                            .getText()
+                            .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
+                            .intern();
+                    String value = new String(elementMore.findElement(By.className(MORE_TEXT))
+                            .getText()
+                            .getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8)
+                            .intern();
+
+                    map.put(key, value);
+                });
+    }
+
+    public void shutdownBot() {
         driver.close();
         driver.quit();
     }
